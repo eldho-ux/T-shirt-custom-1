4bcc1e16c9db35ab3359b005e4f713b705f03b65
eldho-ux <007eldhosaji@gmail.com>
2025-07-14 12:20:26 +0530

    Set ALLOWED_HOSTS to Render domain

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mightytusk.onrender.com']
+ALLOWED_HOSTS = ['https://mightytusk.onrender.com']
 
 
 # Application definition
