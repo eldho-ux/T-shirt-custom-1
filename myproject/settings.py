633f61e0b0a1ee3a30139121a0ace512f2947391
eldho-ux <007eldhosaji@gmail.com>
2025-07-22 17:54:07 +0530

    Initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mightytusk.onrender.com']
+ALLOWED_HOSTS = ['mightytusk.onrender.com','*']
 
 
 # Application definition
